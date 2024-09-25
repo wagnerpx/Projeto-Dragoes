4693c3c60b941e724a98e03f4b08fb8936a05c49
wagnerpx <impussivo@gmail.com>
2024-09-25 15:50:21 -0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+
+const botoesCarrossel = document.querySelectorAll(".botao");
+const imagens = document.querySelectorAll(".imagem");
+const informacoes = document.querySelectorAll(".informacoes");
+
+botoesCarrossel.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    desativarBotaoSelecionado();
+
+    selecionarBotaoCarrossel(botao);
+
+    esconderImagemAtiva();
+    
+    mostrarImagemDeFundo(indice);
+
+    esconderInfomacaoAtiva();
+
+    mostrarInformacoes(indice);
+    
+  });
+});
+function mostrarInformacoes(indice) {
+  informacoes[indice].classList.add("ativa");
+}
+
+function esconderInfomacaoAtiva() {
+  const informacoesAtiva = document.querySelector(".informacoes.ativa");
+  informacoesAtiva.classList.remove("ativa");
+}
+
+function mostrarImagemDeFundo(indice) {
+  imagens[indice].classList.add("ativa");
+}
+
+function selecionarBotaoCarrossel(botao) {
+  botao.classList.add("selecionado");
+}
+
+function esconderImagemAtiva() {
+  const imagemAtiva = document.querySelector(".ativa");
+  imagemAtiva.classList.remove("ativa");
+}
+
+function desativarBotaoSelecionado() {
+  const botaoSelecionado = document.querySelector(".selecionado");
+  botaoSelecionado.classList.remove("selecionado");
+}
+
